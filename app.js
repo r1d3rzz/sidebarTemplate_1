2a12f29b6b4f97873b5e1cdd005d66ae070ac7c9
r1d3rzz <myintthwaykhine127@gmail.com>
2024-09-04 21:55:16 +0630

    customize sidebar icon

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 jQuery(function ($) {
   $(".sidebar-dropdown > a").click(function () {
+    var $thisDropdown = $(this).parent();
+    var $thisIcon = $(this).find("i");
+
     $(".sidebar-submenu").slideUp(200);
-    if ($(this).parent().hasClass("active")) {
-      $(".sidebar-dropdown").removeClass("active");
-      $(this).parent().removeClass("active");
+
+    if ($thisDropdown.hasClass("active")) {
+      $thisDropdown.removeClass("active");
+      $thisIcon.removeClass("fa-folder-open").addClass("fa-folder");
     } else {
       $(".sidebar-dropdown").removeClass("active");
+      $(".sidebar-dropdown i")
+        .removeClass("fa-folder-open")
+        .addClass("fa-folder");
+      $thisDropdown.addClass("active");
+      $thisIcon.removeClass("fa-folder").addClass("fa-folder-open");
       $(this).next(".sidebar-submenu").slideDown(200);
-      $(this).parent().addClass("active");
     }
   });
 
